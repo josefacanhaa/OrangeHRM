45bce4b3e0e0ec444f671af59f7c0c557a01df93
Jose-silvaa <jvitorfacanha@gmail.com>
2026-08-29 17:48:19 -0300

    test

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -7,6 +7,7 @@ export class LoginPage{
   private submitButton: Locator;
 
   constructor(private page: Page) {
+    
     this.usernameInput = page.locator('input[name="username"]')
     this.passwordInput = page.locator('input[type="password"]');
     this.submitButton = page.getByRole('button', { name: 'Login' });
